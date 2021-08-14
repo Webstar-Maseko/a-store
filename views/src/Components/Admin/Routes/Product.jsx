9538b9919ef1b9f43e9a05e0546b5f36d7760f8c
Webstar <webstarsiyabonga@gmail.com>
2021-08-14 12:59:16 +0200

    added filter

--- views/src/Components/Admin/Routes/Product.jsx
+++ views/src/Components/Admin/Routes/Product.jsx
@@ -10,7 +10,8 @@ const Product = () => {
   let [products, setProducts] = useState([]);
   let [category, setCategory] = useState([]);
   let [showModal, setShow] = useState(false);
-
+  let [parent_id, setId] = useState("");
+  let [child_id, setChildId] = useState("");
   function getCategory() {
     axios
       .get("api/category/index")
@@ -24,11 +25,55 @@ const Product = () => {
       .then((res) => setProducts(() => res.data))
       .catch((err) => alert(err));
   }
+  function cateChange(e) {
+    setId(e.target.value);
+     
+  }
+  function tempCat(e){
+    setChildId(e.target.value);
+    console.log(child_id);
+    //getProducts();
 
+    products = products.filter((prod) => prod.category === child_id);
+    
+    if(child_id !== ""){
+      setProducts(products)
+
+    }
+    //else{
+    //   getProducts();
+    // }
+    // console.log(products);
+  }
+  
+  
   useEffect(getProducts, []);
   useEffect(getCategory, []);
   return (
     <div className="pt-5">
+      <div className="text-left">
+        <select name="parent" onChange={cateChange} id="">
+          <option value="">Select category to filter by</option>
+          {category.length > 0 &&
+            category.map((item, index) => (
+              <option key={item._id} value={item._id} >{item.name}</option>
+            ))}
+        </select>
+
+        <select name="child" onChange={tempCat} >
+          <option value="">Select sub-category to filter by</option>
+          {category.length > 0 &&
+            category.map((item, index) => {
+              let  child = item.children.filter((cat) => cat.parentId == parent_id);
+              
+              return child.map((kid,kidInde) => <option key={kid._id} value={kid._id}>{kid.name}</option>)
+
+            }
+              
+            )}
+        </select>
+      </div>
+
       <div className="text-right pb-3">
         <Button onClick={() => setShow(true)}>Add Product</Button>
       </div>
@@ -57,7 +102,7 @@ const Product = () => {
                   {category.map((cat, ind) =>
                     cat.children
                       .filter((child, a) => child._id == item.category)
-                      .map((parent, b) => cat.name +  " > " + parent.name)
+                      .map((parent, b) => cat.name + " > " + parent.name)
                   )}
                 </td>
                 <td>
